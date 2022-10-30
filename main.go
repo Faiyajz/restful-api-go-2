9d39e20cae8963df8f90c6052ecd98db4abdca79
faiyaj <faiyajz007@gmail.com>
2022-10-30 11:49:05 +0600

    create route using gorilla/mux and create empty functions

--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
 
-func main(){
-	fmt.Println("Alhamdulillah")
+	"github.com/gorilla/mux"
+)
+
+type Roll struct {
+	ID          string `json:"id"`
+	ImageNumber string `json:"imageNumber"`
+	Name        string `json:"name"`
+	Ingredients string `json:"ingredients"`
+}
+
+// As we are not using database to store the data,
+// we have created a slice below to store the data
+var rolls []Roll
+
+// Index
+func getRolls(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Show
+func getRoll(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Create
+func createRoll(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Update
+func updateRoll(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Delete
+func deleteRoll(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/sushi", getRolls).Methods("GET")
+	router.HandleFunc("/sushi/{id}", getRoll).Methods("GET")
+	router.HandleFunc("/sushi", createRoll).Methods("POST")
+	router.HandleFunc("/sushi/{id}", updateRoll).Methods("POST")
+	router.HandleFunc("/sushi/{id}", deleteRoll).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":5000", router))
+
 }
